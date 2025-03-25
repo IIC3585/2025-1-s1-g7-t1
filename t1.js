5e796185c79286eb908b40216bdf09ff5207ca20
Camila Gervasoni <camila.gervasoni@uc.cl>
2025-03-25 14:56:46 -0300

    implemented swapColumns y transformToHtmlTable

--- t1.js
+++ t1.js
@@ -14,6 +14,13 @@ const writeCSV = (filepath, data) =>
     "utf-8"
   );
 
+const writeHtml = (filepath, data) =>
+    fs.writeFileSync(
+      filepath,
+      data,
+      "utf-8"
+    );
+
 // Insert Row (file, n, row)
 export const insertRow = (file, n, row) => {};
 
@@ -37,13 +44,41 @@ export const deleteColumn = (file, n) => {
 };
 
 // Swap Columns (file, n, m)
-export const swapColumns = (file, n, m) => {};
+export const swapColumns = (file, n, m) => {
+    const data = readCSV(file);
+    const swap = (row, a, b) => _.concat(_.slice(row, 0, a), _.slice(row, b, b + 1), _.slice(row, a + 1, b), _.slice(row, a, a + 1), _.slice(row, b + 1))
+    const newData = _.map(data, (row) =>
+        n === m
+        ? row
+        : n < m
+            ? swap(row, n - 1, m - 1)
+            : swap(row, m - 1, n - 1)
+      );
+    writeCSV(file, newData);
+};
 
 // Transform to HTML Table (file)
-export const transformToHTMLTable = (file) => {};
+export const transformToHTMLTable = (file) => {
+    const data = readCSV(file);
+    const newData = 
+        `<table>\n` +
+        _.join(_.map(data, (row) => 
+            `    <tr>\n` +
+                _.map(row, (cell) => 
+                    `        <td>${cell}</td>`
+                )
+            .join("\n") +
+            `\n    </tr>`
+        ), "\n") +
+        `\n</table>`;
+    writeHtml("data.html", newData);
+};
+
 
 // Columns to Rows (file )
 export const columnsToRows = (file) => {};
 
 // Rows to Columns (file )
 export const rowsToColumns = (file) => {};
+
+transformToHTMLTable("data.csv");
